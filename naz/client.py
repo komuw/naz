4e5648501057d9710057701e2cf4d85e20a4e540
komuW <komuw05@gmail.com>
2018-06-24 20:04:55 +0300

    use namedTuple

--- naz/client.py
+++ naz/client.py
@@ -2,12 +2,13 @@ import socket
 import struct
 import asyncio
 import logging
-
+import collections
 
 # todo:
 # 1. add configurable retries
 # 2. add configurable rate limits. our rate limits should be tight
 # 3. metrics, what is happening
+
 
 class DefaultSequenceGenerator(object):
     """
@@ -101,69 +102,69 @@ class Client:
         }
 
         # see section 5.1.3 of smpp ver 3.4 spec document
+        CommandStatus = collections.namedtuple('CommandStatus', 'code description')
+
         self.command_statuses = {
-            'ESME_ROK': 0x00000000,  # No Error
-            'ESME_RINVMSGLEN': 0x00000001,  # Message Length is invalid
-            'ESME_RINVCMDLEN': 0x00000002,  # Command Length is invalid
-            'ESME_RINVCMDID': 0x00000003,  # Invalid Command ID
-            'ESME_RINVBNDSTS': 0x00000004,  # Incorrect BIND Status for given command
-            'ESME_RALYBND': 0x00000005,  # ESME Already in Bound State
-            'ESME_RINVPRTFLG': 0x00000006,  # Invalid Priority Flag
-            'ESME_RINVREGDLVFLG': 0x00000007,  # Invalid Registered Delivery Flag
-            'ESME_RSYSERR': 0x00000008,  # System Error
-            'Reserved': 0x00000009,  # Reserved
-            'ESME_RINVSRCADR': 0x0000000A,  # Invalid Source Address
-            'ESME_RINVDSTADR': 0x0000000B,  # Invalid Dest Addr
-            'ESME_RINVMSGID': 0x0000000C,  # Message ID is invalid
-            'ESME_RBINDFAIL': 0x0000000D,  # Bind Failed
-            'ESME_RINVPASWD': 0x0000000E,  # Invalid Password
-            'ESME_RINVSYSID': 0x0000000F,  # Invalid System ID
-            'Reserved': 0x00000010,  # Reserved
-            'ESME_RCANCELFAIL': 0x00000011,  # Cancel SM Failed
-            'Reserved': 0x00000012,  # Reserved
-            'ESME_RREPLACEFAIL': 0x00000013,  # Replace SM Failed
-            'ESME_RMSGQFUL': 0x00000014,  # Message Queue Full
-            'ESME_RINVSERTYP': 0x00000015,  # Invalid Service Type
+            'ESME_ROK': CommandStatus(0x00000000, 'No Error'),
+            'ESME_RINVMSGLEN': CommandStatus(0x00000001, 'Message Length is invalid'),
+            'ESME_RINVCMDLEN': CommandStatus(0x00000002, 'Command Length is invalid'),
+            'ESME_RINVCMDID': CommandStatus(0x00000003, 'Invalid Command ID'),
+            'ESME_RINVBNDSTS': CommandStatus(0x00000004, 'Incorrect BIND Status for given command'),
+            'ESME_RALYBND': CommandStatus(0x00000005, 'ESME Already in Bound State'),
+            'ESME_RINVPRTFLG': CommandStatus(0x00000006, 'Invalid Priority Flag'),
+            'ESME_RINVREGDLVFLG': CommandStatus(0x00000007, 'Invalid Registered Delivery Flag'),
+            'ESME_RSYSERR': CommandStatus(0x00000008, 'System Error'),
+            'Reserved': CommandStatus(0x00000009, 'Reserved'),
+            'ESME_RINVSRCADR': CommandStatus(0x0000000A, 'Invalid Source Address'),
+            'ESME_RINVDSTADR': CommandStatus(0x0000000B, 'Invalid Dest Addr'),
+            'ESME_RINVMSGID': CommandStatus(0x0000000C, 'Message ID is invalid'),
+            'ESME_RBINDFAIL': CommandStatus(0x0000000D, 'Bind Failed'),
+            'ESME_RINVPASWD': CommandStatus(0x0000000E, 'Invalid Password'),
+            'ESME_RINVSYSID': CommandStatus(0x0000000F, 'Invalid System ID'),
+            'Reserved': CommandStatus(0x00000010, 'Reserved'),
+            'ESME_RCANCELFAIL': CommandStatus(0x00000011, 'Cancel SM Failed'),
+            'Reserved': CommandStatus(0x00000012, 'Reserved'),
+            'ESME_RREPLACEFAIL': CommandStatus(0x00000013, 'Replace SM Failed'),
+            'ESME_RMSGQFUL': CommandStatus(0x00000014, 'Message Queue Full'),
+            'ESME_RINVSERTYP': CommandStatus(0x00000015, 'Invalid Service Type'),
             # Reserved 0x00000016 - 0x00000032 Reserved
-            'ESME_RINVNUMDESTS': 0x00000033,  # Invalid number of destinations
-            'ESME_RINVDLNAME': 0x00000034,  # Invalid Distribution List name
+            'ESME_RINVNUMDESTS': CommandStatus(0x00000033, 'Invalid number of destinations'),
+            'ESME_RINVDLNAME': CommandStatus(0x00000034, 'Invalid Distribution List name'),
             # Reserved 0x00000035 - 0x0000003F Reserved
-            'ESME_RINVDESTFLAG': 0x00000040,  # Destination flag is invalid (submit_multi)
-            'Reserved': 0x00000041,  # Reserved
-            # Invalid (submit with replace) request(i.e. submit_sm with replace_if_present_flag set)
-            'ESME_RINVSUBREP': 0x00000042,
-            'ESME_RINVESMCLASS': 0x00000043,  # Invalid esm_class field data
-            'ESME_RCNTSUBDL': 0x00000044,  # Cannot Submit to Distribution List
-            'ESME_RSUBMITFAIL': 0x00000045,  # submit_sm or submit_multi failed
+            'ESME_RINVDESTFLAG': CommandStatus(0x00000040, 'Destination flag is invalid (submit_multi)'),
+            'Reserved': CommandStatus(0x00000041, 'Reserved'),
+            'ESME_RINVSUBREP': CommandStatus(0x00000042, 'Invalid (submit with replace) request(i.e. submit_sm with replace_if_present_flag set)'),
+            'ESME_RINVESMCLASS': CommandStatus(0x00000043, 'Invalid esm_class field data'),
+            'ESME_RCNTSUBDL': CommandStatus(0x00000044, 'Cannot Submit to Distribution List'),
+            'ESME_RSUBMITFAIL': CommandStatus(0x00000045, 'Submit_sm or submit_multi failed'),
             # Reserved 0x00000046 - 0x00000047 Reserved
-            'ESME_RINVSRCTON': 0x00000048,  # Invalid Source address TON
-            'ESME_RINVSRCNPI': 0x00000049,  # Invalid Source address NPI
-            'ESME_RINVDSTTON': 0x00000050,  # Invalid Destination address TON
-            'ESME_RINVDSTNPI': 0x00000051,  # Invalid Destination address NPI
-            'Reserved': 0x00000052,  # Reserved
-            'ESME_RINVSYSTYP': 0x00000053,  # Invalid system_type field
-            'ESME_RINVREPFLAG': 0x00000054,  # Invalid replace_if_present flag
-            'ESME_RINVNUMMSGS': 0x00000055,  # Invalid number of messages
+            'ESME_RINVSRCTON': CommandStatus(0x00000048, 'Invalid Source address TON'),
+            'ESME_RINVSRCNPI': CommandStatus(0x00000049, 'Invalid Source address NPI'),
+            'ESME_RINVDSTTON': CommandStatus(0x00000050, 'Invalid Destination address TON'),
+            'ESME_RINVDSTNPI': CommandStatus(0x00000051, 'Invalid Destination address NPI'),
+            'Reserved': CommandStatus(0x00000052, 'Reserved'),
+            'ESME_RINVSYSTYP': CommandStatus(0x00000053, 'Invalid system_type field'),
+            'ESME_RINVREPFLAG': CommandStatus(0x00000054, 'Invalid replace_if_present flag'),
+            'ESME_RINVNUMMSGS': CommandStatus(0x00000055, 'Invalid number of messages'),
             # Reserved 0x00000056 - 0x00000057 Reserved
-            # Throttling error (ESME has exceeded allowed message limits)
-            'ESME_RTHROTTLED': 0x00000058,
+            'ESME_RTHROTTLED': CommandStatus(0x00000058, 'Throttling error (ESME has exceeded allowed message limits)'),
             # Reserved 0x00000059 - 0x00000060 Reserved
-            'ESME_RINVSCHED': 0x00000061,  # Invalid Scheduled Delivery Time
-            'ESME_RINVEXPIRY': 0x00000062,  # Invalid message validity period (Expiry time)
-            'ESME_RINVDFTMSGID': 0x00000063,  # Predefined Message Invalid or Not Found
-            'ESME_RX_T_APPN': 0x00000064,  # ESME Receiver Temporary App Error Code
-            'ESME_RX_P_APPN': 0x00000065,  # ESME Receiver Permanent App Error Code
-            'ESME_RX_R_APPN': 0x00000066,  # ESME Receiver Reject Message Error Code
-            'ESME_RQUERYFAIL': 0x00000067,  # query_sm request failed
+            'ESME_RINVSCHED': CommandStatus(0x00000061, 'Invalid Scheduled Delivery Time'),
+            'ESME_RINVEXPIRY': CommandStatus(0x00000062, 'Invalid message validity period (Expiry time)'),
+            'ESME_RINVDFTMSGID': CommandStatus(0x00000063, 'Predefined Message Invalid or Not Found'),
+            'ESME_RX_T_APPN': CommandStatus(0x00000064, 'ESME Receiver Temporary App Error Code'),
+            'ESME_RX_P_APPN': CommandStatus(0x00000065, 'ESME Receiver Permanent App Error Code'),
+            'ESME_RX_R_APPN': CommandStatus(0x00000066, 'ESME Receiver Reject Message Error Code'),
+            'ESME_RQUERYFAIL': CommandStatus(0x00000067, 'query_sm request failed'),
             # Reserved 0x00000068 - 0x000000BF Reserved
-            'ESME_RINVOPTPARSTREAM': 0x000000C0,  # Error in the optional part of the PDU Body.
-            'ESME_ROPTPARNOTALLWD': 0x000000C1,  # Optional Parameter not allowed
-            'ESME_RINVPARLEN': 0x000000C2,  # Invalid Parameter Length.
-            'ESME_RMISSINGOPTPARAM': 0x000000C3,  # Expected Optional Parameter missing
-            'ESME_RINVOPTPARAMVAL': 0x000000C4,  # Invalid Optional Parameter Value
+            'ESME_RINVOPTPARSTREAM': CommandStatus(0x000000C0, 'Error in the optional part of the PDU Body.'),
+            'ESME_ROPTPARNOTALLWD': CommandStatus(0x000000C1, 'Optional Parameter not allowed'),
+            'ESME_RINVPARLEN': CommandStatus(0x000000C2, 'Invalid Parameter Length.'),
+            'ESME_RMISSINGOPTPARAM': CommandStatus(0x000000C3, 'Expected Optional Parameter missing'),
+            'ESME_RINVOPTPARAMVAL': CommandStatus(0x000000C4, 'Invalid Optional Parameter Value'),
             # Reserved 0x000000C5 - 0x000000FD Reserved
-            'ESME_RDELIVERYFAILURE': 0x000000FE,  # Delivery Failure (used for data_sm_resp)
-            'ESME_RUNKNOWNERR': 0x000000FF,  # Unknown Error
+            'ESME_RDELIVERYFAILURE': CommandStatus(0x000000FE, 'Delivery Failure (used for data_sm_resp)'),
+            'ESME_RUNKNOWNERR': CommandStatus(0x000000FF, 'Unknown Error'),
             # Reserved for SMPP extension 0x00000100 - 0x000003FF Reserved for SMPP extension
             # Reserved for SMSC vendor specific errors 0x00000400 - 0x000004FF Reserved for SMSC vendor specific errors
             # Reserved 0x00000500 - 0xFFFFFFFF Reserved
@@ -213,7 +214,7 @@ class Client:
         command_length = 16 + len(body)  # 16 is for headers
         command_id = self.command_ids['bind_transceiver']
         # the status for success see section 5.1.3
-        command_status = self.command_statuses['ESME_ROK']
+        command_status = self.command_statuses['ESME_ROK'].code
         sequence_number = self.sequence_generator.next_sequence()
         if sequence_number > self.MAX_SEQUENCE_NUMBER:
             # prevent third party sequence_generators from ruining our party
