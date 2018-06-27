c032fc7db22a2662d88ad3359c2ae1fe8563a9a0
komuW <komuw05@gmail.com>
2018-06-27 20:48:17 +0300

    init tetss

---
+++ naz/tests/test_client.py
@@ -0,0 +1,16 @@
+# the test dir is a sub dir of sewer/sewer so as
+# not to pollute the global namespace.
+# see: https://python-packaging.readthedocs.io/en/latest/testing.html
+
+import mock
+from unittest import TestCase
+
+
+class TestClient(TestCase):
+    """
+    """
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
