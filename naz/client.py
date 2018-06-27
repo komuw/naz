faa02b0cae015ffe817f0938d8319eb1c3753d20
komuW <komuw05@gmail.com>
2018-06-27 16:34:45 +0300

    set max default queue size

--- naz/client.py
+++ naz/client.py
@@ -82,6 +82,7 @@ class Client:
         interface_version=34,
         sequence_generator=None,
         outboundqueue=None,
+        max_outboundqueue_size=10000,
         LOG_LEVEL="DEBUG",
         log_metadata=None,
         codec_class=None,
@@ -140,8 +141,9 @@ class Client:
         if not self.sequence_generator:
             self.sequence_generator = DefaultSequenceGenerator()
         self.outboundqueue = outboundqueue
+        self.max_outboundqueue_size = max_outboundqueue_size
         if not self.outboundqueue:
-            self.outboundqueue = DefaultOutboundQueue(maxsize=5, loop=self.async_loop)
+            self.outboundqueue = DefaultOutboundQueue(maxsize=self.max_outboundqueue_size, loop=self.async_loop)
 
         self.MAX_SEQUENCE_NUMBER = 0x7FFFFFFF
         self.LOG_LEVEL = LOG_LEVEL.upper()
