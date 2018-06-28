b8564a666a239e50202e46c65242ccf55cd1ba30
Komu Wairagu <komuw05@gmail.com>
2018-06-28 09:31:05 +0300

    Update sequence.py

--- naz/sequence.py
+++ naz/sequence.py
@@ -1,6 +1,6 @@
 class DefaultSequenceGenerator(object):
     """
-    sequence_number are 4 octets Integers which allows SMPP requests and responses to be correlated.
+    sequence_number is an Integer of size 4 octets, which allows SMPP requests and responses to be correlated.
     The sequence_number should increase monotonically.
     And they ought to be in the range 0x00000001 to 0x7FFFFFFF
     see section 3.2 of smpp ver 3.4 spec document.
