db1666ca7f614dd3b00bd1bbc1d4e8dc95e17c25
komuW <komuw05@gmail.com>
2018-06-29 00:18:35 +0300

    add hooks

--- naz/hooks.py
+++ naz/hooks.py
@@ -2,6 +2,13 @@ import logging
 
 
 class DefaultHook:
+    """
+    class with hook methods that are called before a request is sent to SMSC and
+    after a response is received from SMSC.
+
+    User's can provide their own hook classes
+    """
+
     def __init__(self, logger=None, LOG_LEVEL="DEBUG"):
         self.LOG_LEVEL = LOG_LEVEL
         self.logger = logger
@@ -15,11 +22,17 @@ class DefaultHook:
             self.logger.setLevel(self.LOG_LEVEL)
 
     async def request(self, event, correlation_id=None):
+        """
+        hook method that is called just before a request is sent to SMSC.
+        """
         self.logger.debug(
             "request_hook_called. event={0}. correlation_id={1}".format(event, correlation_id)
         )
 
     async def response(self, event, correlation_id=None):
+        """
+        hook method that is called just after a response is gotten from SMSC.
+        """
         self.logger.debug(
             "response_hook_called. event={0}. correlation_id={1}".format(event, correlation_id)
         )
