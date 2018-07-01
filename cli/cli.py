3b6fd24a928cbd4e7b52eb84e5708bf50a0516b2
komuW <komuw05@gmail.com>
2018-07-01 13:49:53 +0300

    move a few things

--- cli/cli.py
+++ cli/cli.py
@@ -7,18 +7,6 @@ import argparse
 import naz
 
 
-def load_class(dotted_path):
-    """
-    taken from: https://github.com/coleifer/huey/blob/4138d454cc6fd4d252c9350dbd88d74dd3c67dcb/huey/utils.py#L44
-    huey is released under MIT license a copy of which can be found at: https://github.com/coleifer/huey/blob/master/LICENSE
-    """
-    path, klass = dotted_path.rsplit(".", 1)
-    __import__(path)
-    mod = sys.modules[path]
-    attttr = getattr(mod, klass)
-    return attttr
-
-
 def main():
     """
     """
@@ -127,3 +115,37 @@ def main():
 
 # todo: remove this after testing
 main()
+
+
+def load_class(dotted_path):
+    """
+    taken from: https://github.com/coleifer/huey/blob/4138d454cc6fd4d252c9350dbd88d74dd3c67dcb/huey/utils.py#L44
+    huey is released under MIT license a copy of which can be found at: https://github.com/coleifer/huey/blob/master/LICENSE
+
+    The license is also included below:
+
+    Copyright (c) 2017 Charles Leifer
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+    """
+    path, klass = dotted_path.rsplit(".", 1)
+    __import__(path)
+    mod = sys.modules[path]
+    attttr = getattr(mod, klass)
+    return attttr
