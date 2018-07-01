76528d824500c8124a88600990409f512ff3f551
komuW <komuw05@gmail.com>
2018-07-01 20:52:08 +0300

    start typing

--- naz/q.py
+++ naz/q.py
@@ -1,19 +1,21 @@
 import asyncio
 
+import typing
+
 
 class DefaultOutboundQueue(object):
     """
     this allows users to provide their own queue managers eg redis etc.
     """
 
-    def __init__(self, maxsize, loop):
+    def __init__(self, maxsize: int, loop: asyncio.events.AbstractEventLoop) -> None:
         """
         maxsize is the max number of items(not size) that can be put in the queue.
         """
-        self.queue = asyncio.Queue(maxsize=maxsize, loop=loop)
+        self.queue: asyncio.queues.Queue = asyncio.Queue(maxsize=maxsize, loop=loop)
 
-    async def enqueue(self, item):
+    async def enqueue(self, item: dict) -> None:
         self.queue.put_nowait(item)
 
-    async def dequeue(self):
+    async def dequeue(self) -> typing.Any:
         return await self.queue.get()
