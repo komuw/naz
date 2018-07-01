d10e7b2e7fc2278af630afdbb0692f46ed931cba
komuW <komuw05@gmail.com>
2018-07-01 14:02:35 +0300

    move a few things

--- cli/cli.py
+++ cli/cli.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import json
 import asyncio
@@ -5,6 +6,49 @@ import logging
 import argparse
 
 import naz
+
+
+def load_class(dotted_path):
+    """
+    taken from: https://github.com/coleifer/huey/blob/4138d454cc6fd4d252c9350dbd88d74dd3c67dcb/huey/utils.py#L44
+    huey is released under MIT license a copy of which can be found at: https://github.com/coleifer/huey/blob/master/LICENSE
+
+    The license is also included below:
+
+    Copyright (c) 2017 Charles Leifer
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+    """
+    try:
+        path, klass = dotted_path.rsplit(".", 1)
+        __import__(path)
+        mod = sys.modules[path]
+        attttr = getattr(mod, klass)
+        return attttr
+    except Exception:
+        cur_dir = os.getcwd()
+        if cur_dir not in sys.path:
+            sys.path.insert(0, cur_dir)
+            return load_class(dotted_path)
+        err_mesage = "Error importing {0}".format(dotted_path)
+        sys.stderr.write("\033[91m{0}\033[0m\n".format(err_mesage))
+        raise
 
 
 def main():
@@ -115,37 +159,3 @@ def main():
 
 # todo: remove this after testing
 main()
-
-
-def load_class(dotted_path):
-    """
-    taken from: https://github.com/coleifer/huey/blob/4138d454cc6fd4d252c9350dbd88d74dd3c67dcb/huey/utils.py#L44
-    huey is released under MIT license a copy of which can be found at: https://github.com/coleifer/huey/blob/master/LICENSE
-
-    The license is also included below:
-
-    Copyright (c) 2017 Charles Leifer
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-    THE SOFTWARE.
-    """
-    path, klass = dotted_path.rsplit(".", 1)
-    __import__(path)
-    mod = sys.modules[path]
-    attttr = getattr(mod, klass)
-    return attttr
