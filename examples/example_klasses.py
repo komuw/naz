8302d26a86d5ebe76746c0d2b92698937bb09000
komuW <komuw05@gmail.com>
2018-07-11 12:03:36 +0300

    piych

--- examples/example_klasses.py
+++ examples/example_klasses.py
@@ -1,79 +1,57 @@
 
-import time
+import json
 import asyncio
-import typing
+
 
 import naz
+import redis
 
 
-class ExampleSeqGen(object):
-    """
-    """
-
-    max_sequence_number = 0x7FFFFFFF
-
+class MyRateLimiter(naz.ratelimiter.BaseRateLimiter):
     def __init__(self):
-        self.sequence_number = 1
-
-    def next_sequence(self):
-        if self.sequence_number == self.max_sequence_number:
-            # wrap around
-            self.sequence_number = 1
-        else:
-            self.sequence_number += 1
-        return self.sequence_number
-
-
-class ExampleRateLimiter(naz.ratelimiter.BaseRateLimiter):
-    """
-    Usage:
-        rateLimiter = ExampleRateLimiter(send_rate=10, max_tokens=25)
-        await rateLimiter.limit()
-        send_messsages()
-    """
-
-    def __init__(self, send_rate=1, max_tokens=2, delay_for_tokens=10):
-        self.send_rate = send_rate  # rate in seconds
-        self.max_tokens = max_tokens  # start tokens
-        self.delay_for_tokens = (
-            delay_for_tokens
-        )  # how long(seconds) to wait before checking for token availability after they had finished
-        self.tokens = self.max_tokens
-        self.updated_at = time.monotonic()
+        self.count = 0
 
     async def limit(self):
-        while self.tokens < 1:
-            print(
-                "EXAMPLE_rate_limiting. send_rate={0}. delay_for_tokens={1}".format(
-                    self.send_rate, self.delay_for_tokens
-                )
-            )
-            self.add_new_tokens()
-            await asyncio.sleep(self.delay_for_tokens)
-        self.tokens -= 1
-
-    def add_new_tokens(self):
-        now = time.monotonic()
-        time_since_update = now - self.updated_at
-        new_tokens = time_since_update * self.send_rate
-        if new_tokens > 1:
-            self.tokens = min(self.tokens + new_tokens, self.max_tokens)
-            self.updated_at = now
+        self.count += 1
+        if self.count % 2 == 0:
+            print("\n\t rateLimiter even. sleep={}".format(6))
+            await asyncio.sleep(6)
+        else:
+            print("\n\t rateLimiter odd. sleep={}".format(9))
+            await asyncio.sleep(9)
 
 
 class ExampleQueue(naz.q.BaseOutboundQueue):
-    def __init__(self, maxsize: int = 1000) -> None:
-        """
-        maxsize is the max number of items(not size) that can be put in the queue.
-        """
-        loop: asyncio.events.AbstractEventLoop = asyncio.get_event_loop()
-        self.queue: asyncio.queues.Queue = asyncio.Queue(maxsize=maxsize, loop=loop)
+    def __init__(self):
+        self.redis_instance = redis.StrictRedis(host="localhost", port=6379, db=0)
+        self.queue_name = "myqueue"
 
-    async def enqueue(self, item: dict) -> None:
-        self.queue.put_nowait(item)
+    async def enqueue(self, item):
+        self.redis_instance.lpush(self.queue_name, json.dumps(item))
 
-    async def dequeue(self) -> typing.Dict[typing.Any, typing.Any]:
-        return await self.queue.get()
+    async def dequeue(self):
+        x = self.redis_instance.brpop(self.queue_name)
+        dequed_item = json.loads(x[1].decode())
+        return dequed_item
 
 
-ExampleQueueInstance = ExampleQueue(maxsize=433)
+ExampleQueueInstance = ExampleQueue()
+
+
+def myApp(loop):
+    for i in range(0, 12):
+        print("enque item {}".format(i))
+        item_to_enqueue = {
+            "smpp_event": "submit_sm",
+            "short_message": "Hello World-{0}".format(str(i)),
+            "correlation_id": "myid12345",
+            "source_addr": "254722111111",
+            "destination_addr": "254722999999",
+        }
+        loop.run_until_complete(ExampleQueueInstance.enqueue(item_to_enqueue))
+
+
+# In your Application...
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    myApp(loop)
