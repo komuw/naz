552edc65154e7bee5e3100c9e9e514b48790252f
komuW <komuw05@gmail.com>
2018-12-11 13:51:40 +0300

    hanlde any errors in cli

--- cli/cli.py
+++ cli/cli.py
@@ -57,149 +57,153 @@ def load_class(dotted_path):
 def main():
     """
     """
-    parser = argparse.ArgumentParser(
-        prog="naz",
-        description="""naz is an SMPP client.
-            example usage:
-            naz-cli \
-            --config /path/to/my_config.json
-            """,
-    )
-    parser.add_argument(
-        "--version",
-        action="version",
-        version="%(prog)s {version}".format(version=naz.__version__.about["__version__"]),
-        help="The currently installed naz version.",
-    )
-    parser.add_argument(
-        "--loglevel",
-        type=str,
-        required=False,
-        default="INFO",
-        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
-        help="The log level to output log messages at. \
-        eg: --loglevel DEBUG",
-    )
-    parser.add_argument(
-        "--config",
-        required=True,
-        type=argparse.FileType(mode="r"),
-        help="The config file to use. \
-        eg: --config /path/to/my_config.json",
-    )
-    parser.add_argument(
-        "--dry-run",
-        action="store_true",
-        required=False,
-        default=False,
-        help="""Whether we want to do a dry-run of the naz cli.
-        This is typically only used by developers who are developing naz.
-        eg: --dry-run""",
-    )
-
-    args = parser.parse_args()
-
-    dry_run = args.dry_run
-    config = args.config
-    config_contents = config.read()
-    kwargs = json.loads(config_contents)
-    # todo: validate that config_contents hold all the required params
-    loglevel = kwargs.get("loglevel").upper() if kwargs.get("loglevel") else args.loglevel.upper()
-
-    log_metadata = kwargs.get("log_metadata")
-    if not log_metadata:
-        log_metadata = {}
-    log_metadata.update(
-        {"smsc_host": kwargs.get("smsc_host"), "system_id": kwargs.get("system_id")}
-    )
-
-    extra_log_data = {"log_metadata": log_metadata}
     logger = logging.getLogger("naz.cli")
     handler = logging.StreamHandler()
     formatter = logging.Formatter("%(message)s")
     handler.setFormatter(formatter)
     if not logger.handlers:
         logger.addHandler(handler)
-    logger.setLevel(loglevel)
-    logger = naz.client.NazLoggingAdapter(logger, extra_log_data)
+    logger.setLevel("DEBUG")
+    logger.info({"event": "naz.cli", "stage": "start"})
 
-    logger.info("\n\n\t {} \n\n".format("Naz: the SMPP client."))
-    if dry_run:
-        logger.warn(
-            "\n\n\t {} \n\n".format(
-                "Naz: Caution; You have activated dry-run, naz may not behave correctly."
-            )
+    loop = asyncio.get_event_loop()
+    try:
+        parser = argparse.ArgumentParser(
+            prog="naz",
+            description="""naz is an SMPP client.
+                example usage:
+                naz-cli \
+                --config /path/to/my_config.json
+                """,
+        )
+        parser.add_argument(
+            "--version",
+            action="version",
+            version="%(prog)s {version}".format(version=naz.__version__.about["__version__"]),
+            help="The currently installed naz version.",
+        )
+        parser.add_argument(
+            "--loglevel",
+            type=str,
+            required=False,
+            default="INFO",
+            choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+            help="The log level to output log messages at. \
+            eg: --loglevel DEBUG",
+        )
+        parser.add_argument(
+            "--config",
+            required=True,
+            type=argparse.FileType(mode="r"),
+            help="The config file to use. \
+            eg: --config /path/to/my_config.json",
+        )
+        parser.add_argument(
+            "--dry-run",
+            action="store_true",
+            required=False,
+            default=False,
+            help="""Whether we want to do a dry-run of the naz cli.
+            This is typically only used by developers who are developing naz.
+            eg: --dry-run""",
         )
 
-    # Load custom classes #######################
-    # Users can ONLY pass in:
-    # 1. python class instances
-    # if the thing that the user passed in is a python class, we need to exit with an error
-    # we'll use `inspect.isclass` to do that
-    # todo: test the h** out of this logic
+        args = parser.parse_args()
 
-    outboundqueue = load_class(kwargs["outboundqueue"])  # this is a mandatory param
-    kwargs["outboundqueue"] = outboundqueue
-    if inspect.isclass(outboundqueue):
-        # DO NOT instantiate class instance, fail with appropriate error instead.
-        logger.exception("\n\n\t {} \n\n".format("outboundqueue should be a class instance."))
-        sys.exit(77)
+        dry_run = args.dry_run
+        config = args.config
+        config_contents = config.read()
+        kwargs = json.loads(config_contents)
 
-    sequence_generator = kwargs.get("sequence_generator")
-    if sequence_generator:
-        sequence_generator = load_class(sequence_generator)
-        # kwargs should contain the actual loaded class instances
-        kwargs["sequence_generator"] = sequence_generator
-        if inspect.isclass(sequence_generator):
-            logger.exception(
-                "\n\n\t {} \n\n".format("sequence_generator should be a class instance.")
+        # todo: validate that config_contents hold all the required params
+        loglevel = (
+            kwargs.get("loglevel").upper() if kwargs.get("loglevel") else args.loglevel.upper()
+        )
+        logger.setLevel(loglevel)
+        log_metadata = kwargs.get("log_metadata")
+        if not log_metadata:
+            log_metadata = {}
+        log_metadata.update(
+            {"smsc_host": kwargs.get("smsc_host"), "system_id": kwargs.get("system_id")}
+        )
+        extra_log_data = {"log_metadata": log_metadata}
+        logger = naz.client.NazLoggingAdapter(logger, extra_log_data)
+
+        logger.info("\n\n\t {} \n\n".format("Naz: the SMPP client."))
+        if dry_run:
+            logger.warn(
+                "\n\n\t {} \n\n".format(
+                    "Naz: Caution; You have activated dry-run, naz may not behave correctly."
+                )
             )
+
+        # Load custom classes #######################
+        # Users can ONLY pass in:
+        # 1. python class instances
+        # if the thing that the user passed in is a python class, we need to exit with an error
+        # we'll use `inspect.isclass` to do that
+        # todo: test the h** out of this logic
+
+        outboundqueue = load_class(kwargs["outboundqueue"])  # this is a mandatory param
+        kwargs["outboundqueue"] = outboundqueue
+        if inspect.isclass(outboundqueue):
+            # DO NOT instantiate class instance, fail with appropriate error instead.
+            logger.exception("\n\n\t {} \n\n".format("outboundqueue should be a class instance."))
             sys.exit(77)
 
-    codec_class = kwargs.get("codec_class")
-    if codec_class:
-        codec_class = load_class(codec_class)
-        kwargs["codec_class"] = codec_class
-        if inspect.isclass(codec_class):
-            logger.exception("\n\n\t {} \n\n".format("codec_class should be a class instance."))
-            sys.exit(77)
-    rateLimiter = kwargs.get("rateLimiter")
-    if rateLimiter:
-        rateLimiter = load_class(rateLimiter)
-        kwargs["rateLimiter"] = rateLimiter
-        if inspect.isclass(rateLimiter):
-            logger.exception("\n\n\t {} \n\n".format("rateLimiter should be a class instance."))
-            sys.exit(77)
-    hook = kwargs.get("hook")
-    if hook:
-        hook = load_class(hook)
-        kwargs["hook"] = hook
-        if inspect.isclass(hook):
-            logger.exception("\n\n\t {} \n\n".format("hook should be a class instance."))
-            sys.exit(77)
-    throttle_handler = kwargs.get("throttle_handler")
-    if throttle_handler:
-        throttle_handler = load_class(throttle_handler)
-        kwargs["throttle_handler"] = throttle_handler
-        if inspect.isclass(throttle_handler):
-            logger.exception(
-                "\n\n\t {} \n\n".format("throttle_handler should be a class instance.")
-            )
-            sys.exit(77)
-    # Load custom classes #######################
+        sequence_generator = kwargs.get("sequence_generator")
+        if sequence_generator:
+            sequence_generator = load_class(sequence_generator)
+            # kwargs should contain the actual loaded class instances
+            kwargs["sequence_generator"] = sequence_generator
+            if inspect.isclass(sequence_generator):
+                logger.exception(
+                    "\n\n\t {} \n\n".format("sequence_generator should be a class instance.")
+                )
+                sys.exit(77)
 
-    # call naz api ##############################
-    loop = asyncio.get_event_loop()
-    cli = naz.Client(async_loop=loop, **kwargs)
-    if dry_run:
-        return
+        codec_class = kwargs.get("codec_class")
+        if codec_class:
+            codec_class = load_class(codec_class)
+            kwargs["codec_class"] = codec_class
+            if inspect.isclass(codec_class):
+                logger.exception("\n\n\t {} \n\n".format("codec_class should be a class instance."))
+                sys.exit(77)
+        rateLimiter = kwargs.get("rateLimiter")
+        if rateLimiter:
+            rateLimiter = load_class(rateLimiter)
+            kwargs["rateLimiter"] = rateLimiter
+            if inspect.isclass(rateLimiter):
+                logger.exception("\n\n\t {} \n\n".format("rateLimiter should be a class instance."))
+                sys.exit(77)
+        hook = kwargs.get("hook")
+        if hook:
+            hook = load_class(hook)
+            kwargs["hook"] = hook
+            if inspect.isclass(hook):
+                logger.exception("\n\n\t {} \n\n".format("hook should be a class instance."))
+                sys.exit(77)
+        throttle_handler = kwargs.get("throttle_handler")
+        if throttle_handler:
+            throttle_handler = load_class(throttle_handler)
+            kwargs["throttle_handler"] = throttle_handler
+            if inspect.isclass(throttle_handler):
+                logger.exception(
+                    "\n\n\t {} \n\n".format("throttle_handler should be a class instance.")
+                )
+                sys.exit(77)
+        # Load custom classes #######################
 
-    # connect to the SMSC host
-    reader, writer = loop.run_until_complete(cli.connect())
-    # bind to SMSC as a tranceiver
-    loop.run_until_complete(cli.tranceiver_bind())
+        # call naz api ##############################
+        cli = naz.Client(async_loop=loop, **kwargs)
+        if dry_run:
+            return
 
-    try:
+        # connect to the SMSC host
+        reader, writer = loop.run_until_complete(cli.connect())
+        # bind to SMSC as a tranceiver
+        loop.run_until_complete(cli.tranceiver_bind())
+
         # read any data from SMSC, send any queued messages to SMSC and continually check the state of the SMSC
         tasks = asyncio.gather(
             cli.send_forever(TESTING=dry_run),
@@ -207,11 +211,11 @@ def main():
             cli.enquire_link(TESTING=dry_run),
         )
         loop.run_until_complete(tasks)
-        loop.run_forever()
+        loop.run_until_complete(cli.unbind())
     except Exception as e:
         logger.exception({"event": "naz.cli", "stage": "end", "error": str(e)})
     finally:
-        loop.run_until_complete(cli.unbind())
+        logger.info({"event": "naz.cli", "stage": "end"})
         loop.close()
     # call naz api #################################
 
