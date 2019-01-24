fdca5d4e5f4020c1b1d0fa91e5aebe7f11f9a8b8
komuw <komuw05@gmail.com>
2019-01-24 19:27:04 +0300

    add SmppCommandStatus:

--- naz/client.py
+++ naz/client.py
@@ -1491,3 +1491,197 @@ class SmppCommand:
     ENQUIRE_LINK = "enquire_link"
     ENQUIRE_LINK_RESP = "enquire_link_resp"
     GENERIC_NACK = "generic_nack"
+
+
+_commandStatus = collections.namedtuple("CommandStatus", "code value description")
+
+
+class SmppCommandStatus:
+    """
+    see section 5.1.3 of smpp ver 3.4 spec document
+    """
+
+    ESME_ROK = _commandStatus(code="ESME_ROK", value=0x00000000, description="Success")
+    ESME_RINVMSGLEN = _commandStatus(
+        code="ESME_RINVMSGLEN", value=0x00000001, description="Message Length is invalid"
+    )
+    ESME_RINVCMDLEN = _commandStatus(
+        code="ESME_RINVCMDLEN", value=0x00000002, description="Command Length is invalid"
+    )
+    ESME_RINVCMDID = _commandStatus(
+        code="ESME_RINVCMDID", value=0x00000003, description="Invalid Command ID"
+    )
+    ESME_RINVBNDSTS = _commandStatus(
+        code="ESME_RINVBNDSTS",
+        value=0x00000004,
+        description="Incorrect BIND Status for given command",
+    )
+    ESME_RALYBND = _commandStatus(
+        code="ESME_RALYBND", value=0x00000005, description="ESME Already in Bound State"
+    )
+    ESME_RINVPRTFLG = _commandStatus(
+        code="ESME_RINVPRTFLG", value=0x00000006, description="Invalid Priority Flag"
+    )
+    ESME_RINVREGDLVFLG = _commandStatus(
+        code="ESME_RINVREGDLVFLG", value=0x00000007, description="Invalid Registered Delivery Flag"
+    )
+    ESME_RSYSERR = _commandStatus(code="ESME_RSYSERR", value=0x00000008, description="System Error")
+    # Reserved =  _commandStatus(code="Reserved", value=0x00000009,description= "Reserved")
+    ESME_RINVSRCADR = _commandStatus(
+        code="ESME_RINVSRCADR", value=0x0000000A, description="Invalid Source Address"
+    )
+    ESME_RINVSRCADR = _commandStatus(
+        code="ESME_RINVSRCADR", value=0x0000000A, description="Invalid Source Address"
+    )
+    ESME_RINVDSTADR = _commandStatus(
+        code="ESME_RINVDSTADR", value=0x0000000B, description="Invalid Dest Addr"
+    )
+    ESME_RINVMSGID = _commandStatus(
+        code="ESME_RINVMSGID", value=0x0000000C, description="Message ID is invalid"
+    )
+    ESME_RBINDFAIL = _commandStatus(
+        code="ESME_RBINDFAIL", value=0x0000000D, description="Bind Failed"
+    )
+    ESME_RINVPASWD = _commandStatus(
+        code="ESME_RINVPASWD", value=0x0000000E, description="Invalid Password"
+    )
+    ESME_RINVSYSID = _commandStatus(
+        code="ESME_RINVSYSID", value=0x0000000F, description="Invalid System ID"
+    )
+    # Reserved =  _commandStatus(code="Reserved", value=0x00000010,description= "Reserved")
+    ESME_RCANCELFAIL = _commandStatus(
+        code="ESME_RCANCELFAIL", value=0x00000011, description="Cancel SM Failed"
+    )
+    # Reserved =  _commandStatus(code="Reserved", value=0x00000012,description= "Reserved")
+    ESME_RREPLACEFAIL = _commandStatus(
+        code="ESME_RREPLACEFAIL", value=0x00000013, description="Replace SM Failed"
+    )
+    ESME_RMSGQFUL = _commandStatus(
+        code="ESME_RMSGQFUL", value=0x00000014, description="Message Queue Full"
+    )
+    ESME_RINVSERTYP = _commandStatus(
+        code="ESME_RINVSERTYP", value=0x00000015, description="Invalid Service Type"
+    )
+    # Reserved 0x00000016 - 0x00000032 Reserved
+    ESME_RINVNUMDESTS = _commandStatus(
+        code="ESME_RINVNUMDESTS", value=0x00000033, description="Invalid number of destinations"
+    )
+    ESME_RINVDLNAME = _commandStatus(
+        code="ESME_RINVNUMDESTS", value=0x00000034, description="Invalid Distribution List name"
+    )
+    # Reserved 0x00000035 - 0x0000003F Reserved
+    ESME_RINVDESTFLAG = _commandStatus(
+        code="ESME_RINVDESTFLAG",
+        value=0x00000040,
+        description="Destination flag is invalid (submit_multi)",
+    )
+    # Reserved =  _commandStatus(code="Reserved", value=0x00000041,description= "Reserved")
+    ESME_RINVSUBREP = _commandStatus(
+        code="ESME_RINVSUBREP",
+        value=0x00000042,
+        description="Invalid (submit with replace) request(i.e. submit_sm with replace_if_present_flag set)",
+    )
+    ESME_RINVESMCLASS = _commandStatus(
+        code="ESME_RINVESMCLASS", value=0x00000043, description="Invalid esm_class field data"
+    )
+    ESME_RCNTSUBDL = _commandStatus(
+        code="ESME_RCNTSUBDL", value=0x00000044, description="Cannot Submit to Distribution List"
+    )
+    ESME_RSUBMITFAIL = _commandStatus(
+        code="ESME_RSUBMITFAIL", value=0x00000045, description="Submit_sm or submit_multi failed"
+    )
+    # Reserved 0x00000046 - 0x00000047 Reserved
+    ESME_RINVSRCTON = _commandStatus(
+        code="ESME_RINVSRCTON", value=0x00000048, description="Invalid Source address TON"
+    )
+    ESME_RINVSRCNPI = _commandStatus(
+        code="ESME_RINVSRCNPI", value=0x00000049, description="Invalid Source address NPI"
+    )
+    ESME_RINVDSTTON = _commandStatus(
+        code="ESME_RINVDSTTON", value=0x00000050, description="Invalid Destination address TON"
+    )
+    ESME_RINVDSTNPI = _commandStatus(
+        code="ESME_RINVDSTNPI", value=0x00000051, description="Invalid Destination address NPI"
+    )
+    # Reserved =  _commandStatus(code="Reserved", value=0x00000052,description= "Reserved")
+    ESME_RINVSYSTYP = _commandStatus(
+        code="ESME_RINVSYSTYP", value=0x00000053, description="Invalid system_type field"
+    )
+    ESME_RINVREPFLAG = _commandStatus(
+        code="ESME_RINVREPFLAG", value=0x00000054, description="Invalid replace_if_present flag"
+    )
+    ESME_RINVNUMMSGS = _commandStatus(
+        code="ESME_RINVNUMMSGS", value=0x00000055, description="Invalid number of messages"
+    )
+    # Reserved 0x00000056 - 0x00000057 Reserved
+    ESME_RTHROTTLED = _commandStatus(
+        code="ESME_RTHROTTLED",
+        value=0x00000058,
+        description="Throttling error (ESME has exceeded allowed message limits)",
+    )
+    # Reserved 0x00000059 - 0x00000060 Reserved
+    ESME_RINVSCHED = _commandStatus(
+        code="ESME_RINVSCHED", value=0x00000061, description="Invalid Scheduled Delivery Time"
+    )
+    ESME_RINVEXPIRY = _commandStatus(
+        code="ESME_RINVEXPIRY",
+        value=0x00000062,
+        description="Invalid message validity period (Expiry time)",
+    )
+    ESME_RINVDFTMSGID = _commandStatus(
+        code="ESME_RINVDFTMSGID",
+        value=0x00000063,
+        description="Predefined Message Invalid or Not Found",
+    )
+    ESME_RX_T_APPN = _commandStatus(
+        code="ESME_RX_T_APPN",
+        value=0x00000064,
+        description="ESME Receiver Temporary App Error Code",
+    )
+    ESME_RX_P_APPN = _commandStatus(
+        code="ESME_RX_P_APPN",
+        value=0x00000065,
+        description="ESME Receiver Permanent App Error Code",
+    )
+    ESME_RX_R_APPN = _commandStatus(
+        code="ESME_RX_R_APPN",
+        value=0x00000066,
+        description="ESME Receiver Reject Message Error Code",
+    )
+    ESME_RQUERYFAIL = _commandStatus(
+        code="ESME_RQUERYFAIL", value=0x00000067, description="query_sm request failed"
+    )
+    # Reserved 0x00000068 - 0x000000BF Reserved
+    ESME_RINVOPTPARSTREAM = _commandStatus(
+        code="ESME_RINVOPTPARSTREAM",
+        value=0x000000C0,
+        description="Error in the optional part of the PDU Body.",
+    )
+    ESME_ROPTPARNOTALLWD = _commandStatus(
+        code="ESME_ROPTPARNOTALLWD", value=0x000000C1, description="Optional Parameter not allowed"
+    )
+    ESME_RINVPARLEN = _commandStatus(
+        code="ESME_RINVPARLEN", value=0x000000C2, description="Invalid Parameter Length."
+    )
+    ESME_RMISSINGOPTPARAM = _commandStatus(
+        code="ESME_RMISSINGOPTPARAM",
+        value=0x000000C3,
+        description="Expected Optional Parameter missing",
+    )
+    ESME_RINVOPTPARAMVAL = _commandStatus(
+        code="ESME_RINVOPTPARAMVAL",
+        value=0x000000C4,
+        description="Invalid Optional Parameter Value",
+    )
+    # Reserved 0x000000C5 - 0x000000FD Reserved
+    ESME_RDELIVERYFAILURE = _commandStatus(
+        code="ESME_RDELIVERYFAILURE",
+        value=0x000000FE,
+        description="Delivery Failure (used for data_sm_resp)",
+    )
+    ESME_RUNKNOWNERR = _commandStatus(
+        code="ESME_RUNKNOWNERR", value=0x000000FF, description="Unknown Error"
+    )
+    # Reserved for SMPP extension 0x00000100 - 0x000003FF Reserved for SMPP extension
+    # Reserved for SMSC vendor specific errors 0x00000400 - 0x000004FF Reserved for SMSC vendor specific errors
+    # Reserved 0x00000500 - 0xFFFFFFFF Reserved
