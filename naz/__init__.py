0a747e1639cf1e8d78e2459ab6a12c8d8c10b286
komuW <komuw05@gmail.com>
2019-01-28 18:27:31 +0300

    pip install pdoc3 pdoc --html --html-dir pdcoc-html/ --overwrite --external-links naz

--- naz/__init__.py
+++ naz/__init__.py
@@ -1,3 +1,23 @@
+"""
+`naz` is an async SMPP client.
+
+It's name is derived from Kenyan hip hop artiste, Nazizi.
+
+`SMPP is a protocol designed for the transfer of short message data between External Short Messaging Entities(ESMEs), Routing Entities(REs) and Short Message Service Center(SMSC).` - [Wikipedia]
+
+naz currently only supports SMPP version 3.4.
+
+naz has no third-party dependencies and it requires python version 3.6+
+
+naz is in active development and it's API may change in backward incompatible ways.
+
+https://pypi.python.org/pypi/naz
+
+
+
+[Wikipedia]: https://en.wikipedia.org/wiki/Short_Message_Peer-to-Peer
+
+"""
 from .client import Client  # noqa: F401
 from .client import SmppCommand  # noqa: F401
 from .client import SmppDataCoding  # noqa: F401
