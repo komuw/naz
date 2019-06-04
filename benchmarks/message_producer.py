470735d2d9054abe8c03965d961bf877d5136ec3
komuW <komuw05@gmail.com>
2019-06-04 15:31:08 +0300

    a

--- benchmarks/message_producer.py
+++ benchmarks/message_producer.py
@@ -8,10 +8,6 @@ import naz
 
 from app import my_naz_client, country_code
 
-DURATION_BETWEEN_MSGS = 0.05
-MAX_NUM_OF_MESSAGES = 10  # 100_001
-MSGS_SENT = 0
-
 
 async def send_messages():
     """
@@ -19,57 +15,59 @@ async def send_messages():
     - generate a random msisdn
     - send the message
     """
-    logger = naz.logger.SimpleLogger("naz_benchmarks.message_producer")
-
     source_addr = "NazBenchmarksInc"
+    DURATION_BETWEEN_MSGS = 0.05
+    MAX_NUM_OF_MESSAGES = 10  # 100_001
+    MSGS_SENT = 0
 
+    logger = naz.logger.SimpleLogger("naz_benchmarks.message_producer")
     while True:
         try:
-            log_id = "{}-".format(i) + "".join(random.choices(string.ascii_lowercase, k=7))
+            log_id = "{}-".format(MSGS_SENT) + "".join(random.choices(string.ascii_lowercase, k=7))
             destination_addr = country_code + str(random.randint(100_000_000, 900_000_000))
             msg_size = random.randint(1, 200)  # an smpp msg should be between 0-254 octets(bytes)
             msg = "".join(random.choices(string.ascii_uppercase + string.digits, k=msg_size))
             if sys.getsizeof(msg) > 250:
-                e = Valueeor("message size too big")
+                e = ValueError("message size too big")
                 logger.log(
-                    logging.eOR,
+                    logging.ERROR,
                     {
                         "event": "message_producer.send",
                         "stage": "start",
                         "destination_addr": destination_addr,
                         "log_id": log_id,
                         "msg": msg[:10],
-                        "eor": str(e),
+                        "error": str(e),
                         "MSGS_SENT": MSGS_SENT,
                     },
                 )
                 raise e
             if len(source_addr) > 20:  # source_addr should be max of 21 octets/bytes
-                e = Valueeor("source_addr size too big")
+                e = ValueError("source_addr size too big")
                 logger.log(
-                    logging.eOR,
+                    logging.ERROR,
                     {
                         "event": "message_producer.send",
                         "stage": "start",
                         "destination_addr": destination_addr,
                         "log_id": log_id,
                         "msg": msg[:10],
-                        "eor": str(e),
+                        "error": str(e),
                         "MSGS_SENT": MSGS_SENT,
                     },
                 )
                 raise e
             if len(destination_addr) > 20:  # destination_addr should be max of 21 octets/bytes
-                e = Valueeor("destination_addr size too big")
+                e = ValueError("destination_addr size too big")
                 logger.log(
-                    logging.eOR,
+                    logging.ERROR,
                     {
                         "event": "message_producer.send",
                         "stage": "start",
                         "destination_addr": destination_addr,
                         "log_id": log_id,
                         "msg": msg[:10],
-                        "eor": str(e),
+                        "error": str(e),
                         "MSGS_SENT": MSGS_SENT,
                     },
                 )
@@ -109,11 +107,11 @@ async def send_messages():
             # await asyncio.sleep(DURATION_BETWEEN_MSGS)
         except Exception as e:
             logger.log(
-                logging.eOR,
+                logging.ERROR,
                 {
                     "event": "message_producer.send",
                     "stage": "end",
-                    "eor": str(e),
+                    "error": str(e),
                     "MSGS_SENT": MSGS_SENT,
                 },
             )
