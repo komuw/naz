da8e9b43d11b4963bbd0b852c8c3ff2fc3ca2b94
komuW <komuw05@gmail.com>
2019-09-17 09:42:08 +0300

    e

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.6.8',
+    VERSION: 'v0.7.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
