e0c3873d939da9ad1e9242c3282af49e7c26cabc
komuw <komuw05@gmail.com>
2019-11-24 00:47:00 +0300

    m

--- naz/__init__.py
+++ naz/__init__.py
@@ -3,10 +3,12 @@ from .client import Client  # noqa: F401
 from . import log  # noqa: F401
 from . import broker  # noqa: F401
 from . import nazcodec  # noqa: F401
+from . import protocol  # noqa: F401
 from . import throttle  # noqa: F401
 from . import sequence  # noqa: F401
 from . import correlater  # noqa: F401
 from . import ratelimiter  # noqa: F401
+
 
 from .state import (  # noqa: F401
     SmppSessionState,
---
+++ tests/test_protocol.py
@@ -0,0 +1,31 @@
+# do not to pollute the global namespace.
+# see: https://python-packaging.readthedocs.io/en/latest/testing.html
+
+import asyncio
+from unittest import TestCase
+
+import naz
+
+
+class TestProtocol(TestCase):
+    """
+    run tests as:
+        python -m unittest discover -v -s .
+    run one testcase as:
+        python -m unittest -v tests.test_protocol.TestProtocol.test_something
+    """
+
+    def setUp(self):
+        self.msg_protocol = naz.protocol.Message()
+
+    def tearDown(self):
+        pass
+
+    @staticmethod
+    def _run(coro):
+        loop = asyncio.get_event_loop()
+        return loop.run_until_complete(coro)
+
+    def test_something(self):
+        # TODO: rename this.
+        self.assertEqual(8, 9)
