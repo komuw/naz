2f995a2a625711b19eb45b717ae0c8ddd56c4020
komuw <komuw05@gmail.com>
2019-11-24 11:57:35 +0300

    m

--- naz/__version__.py
+++ naz/__version__.py
@@ -2,7 +2,7 @@ about = {
     "__title__": "naz",
     "__description__": "Naz is an async SMPP client.",
     "__url__": "https://github.com/komuw/naz",
-    "__version__": "v0.7.3",
+    "__version__": "v0.7.4",
     "__author__": "komuW",
     "__author_email__": "komuw05@gmail.com",
     "__license__": "MIT",
