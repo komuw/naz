0956a18ba8d25ac613669797fde30e012a0233f7
komuw <komuw05@gmail.com>
2019-11-24 14:36:00 +0300

    m

--- examples/example_config.py
+++ examples/example_config.py
@@ -37,7 +37,62 @@ async def asyncMain(buf):
     # zzz = buf.decode()
     await client.connect()
     client.current_session_state = naz.SmppSessionState.BOUND_TRX
-    await client._parse_response_pdu(pdu=buf)
+
+    # 1.
+    _msg_to_log(buf)
+
+    # 2.
+    await send_data(
+        smpp_command="smpp_command", msg=buf, log_id="log_id", hook_metadata="hook_metadata"
+    )
+
+    # 3.
+    await _parse_response_pdu(pdu=buf)
+
+    # 4.
+    await command_handlers(
+        pdu=buf,
+        body_data=body_data,
+        smpp_command=smpp_command,
+        command_status_value=5,
+        sequence_number=78,
+        log_id="log_id",
+        hook_metadata="hook_metadata",
+    )
+
+
+def _msg_to_log(msg: bytes):
+    client._msg_to_log(msg=msg)
+
+
+async def send_data(smpp_command: str, msg: bytes, log_id: str, hook_metadata: str = ""):
+    await client.send_data(
+        smpp_command=smpp_command, msg=msg, log_id=log_id, hook_metadata=hook_metadata
+    )
+
+
+async def _parse_response_pdu(pdu: bytes) -> None:
+    await client._parse_response_pdu(pdu=pdu)
+
+
+async def command_handlers(
+    pdu: bytes,
+    body_data: bytes,
+    smpp_command: str,
+    command_status_value: int,
+    sequence_number: int,
+    log_id: str,
+    hook_metadata: str,
+) -> None:
+    await client.command_handlers(
+        pdu=pdu,
+        body_data=body_data,
+        smpp_command=smpp_command,
+        command_status_value=command_status_value,
+        sequence_number=sequence_number,
+        log_id=log_id,
+        hook_metadata=hook_metadata,
+    )
 
 
 if __name__ == "__main__":
