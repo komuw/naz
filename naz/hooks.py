468cbd8186f73100a909d7f6c1667780a58888e3
komuw <komuw05@gmail.com>
2019-11-28 17:28:57 +0300

    simple logger can log bytes

--- naz/hooks.py
+++ naz/hooks.py
@@ -80,6 +80,7 @@ class SimpleHook(BaseHook):
                 "smpp_command": smpp_command,
                 "log_id": log_id,
                 "hook_metadata": hook_metadata,
+                # SimpleLogger cant log bytes(pdu)
             },
         )
 
