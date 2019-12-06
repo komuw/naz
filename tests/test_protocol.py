a94ea4c0ebde8baa439c91ddeeb02d76a93f52dc
komuw <komuw05@gmail.com>
2019-12-06 09:50:33 +0300

    r

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -26,20 +26,6 @@ class TestProtocol(TestCase):
         )
         self.assertIsNotNone(proto)
 
-    def test_pdu_N_shortMsg_exclusive(self):
-        with self.assertRaises(ValueError) as raised_exception:
-            naz.protocol.SubmitSM(
-                version=1,
-                smpp_command=naz.SmppCommand.SUBMIT_SM,
-                log_id="some-log-id",
-                pdu=b"some-pdu",
-                short_message="some-msg",
-            )
-        self.assertIsInstance(raised_exception.exception, ValueError)
-        self.assertIn(
-            "You cannot specify both `pdu` and `short_message`", str(raised_exception.exception)
-        )
-
     def test_json_serialization(self):
         proto = naz.protocol.SubmitSM(
             version=1,
@@ -77,27 +63,14 @@ class TestProtocol(TestCase):
 
         self.assertIsInstance(proto, naz.protocol.Message)
 
-    def test_protocol_message_ENCODING(self):
-        """
-        test that even though `naz.protocol.Message.pdu` make contain smpp fields that are encoded
-        using different encoding schemes(ascii, ucs2, struct.pack etc);
-        it is still okay to use the `naz.protocol.Message.ENCODING` on them.
-        And that when you encode and decode them using `naz.protocol.Message.ENCODING`, they will come out alright.
-        """
-        # pdu that contains some fields that have been
-        # encoded with `ascii` and other fields encoded with `ucs2`
-        full_pdu = b"\x00\x00\x00V\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x05CMT\x00\x01\x01254722111111\x00\x01\x01254722999999\x00\x03\x00\x00\x00\x00\x01\x00\x08\x00\x1a\x00H\x00e\x00l\x00l\x00o\x00 \x00W\x00o\x00r\x00l\x00d\x00-\x000"
-        self.assertEqual(
-            full_pdu.decode(naz.protocol.Message.ENCODING).encode(naz.protocol.Message.ENCODING),
-            full_pdu,
-        )
-
-        proto = naz.protocol.Message(
-            version=1, smpp_command=naz.SmppCommand.SUBMIT_SM, log_id="some-log-id", pdu=full_pdu
-        )
+    def test_serialize_n_deserialize(self):
+        proto = naz.protocol.EnquireLinkResp(log_id="some-log-id", sequence_number=294)
         _in_json = proto.to_json()
         self.assertIsNotNone(_in_json)
 
-        new_proto = naz.protocol.Message.from_json(_in_json)
+        new_proto = naz.protocol.json_to_Message(_in_json)
         self.assertEqual(type(proto), type(new_proto))
-        self.assertEqual(new_proto.pdu, full_pdu)
+        self.assertEqual(new_proto.log_id, proto.log_id)
+        self.assertEqual(new_proto.sequence_number, proto.sequence_number)
+        self.assertEqual(new_proto.smpp_command, naz.state.SmppCommand.ENQUIRE_LINK_RESP)
+        self.assertEqual(new_proto.smpp_command, naz.state.SmppCommand.ENQUIRE_LINK_RESP)
